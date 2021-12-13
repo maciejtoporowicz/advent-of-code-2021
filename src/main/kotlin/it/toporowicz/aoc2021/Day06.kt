35e9eaa01b2f78920680a76d77fe743b3432bbd4
Maciej Toporowicz <maciej.toporowicz@gmail.com>
2021-12-14 00:02:19 +0100

    readability improvements

--- src/main/kotlin/it/toporowicz/aoc2021/Day06.kt
+++ src/main/kotlin/it/toporowicz/aoc2021/Day06.kt
@@ -29,7 +29,7 @@ class Day06 {
 
     data class FishSchool constructor(private val numberOfFishByInternalTimer: Map<InternalTimer, NumberOfFish>) {
         constructor(allFish: List<Fish>) : this(
-            (0..8).associateWith { internalTimerValue ->
+            (MIN_INTERNAL_TIMER..MAX_INTERNAL_TIMER).associateWith { internalTimerValue ->
                 allFish.count { it.internalTimer == internalTimerValue }.toLong()
             }
         )
@@ -40,17 +40,31 @@ class Day06 {
             numberOfFishByInternalTimer
                 .toMutableMap()
                 .also {
-                    val numberOfNewbornFish = it.getValue(0)
+                    val numberOfNewbornFish = it.getValue(MIN_INTERNAL_TIMER)
 
-                    (1..8).forEach { internalTimerValue ->
-                        it[internalTimerValue - 1] = it.getValue(internalTimerValue)
-                    }
-                    it[6] = it.getValue(6) + numberOfNewbornFish
-                    it[8] = numberOfNewbornFish
+                    it.keys.sorted()
+                        .drop(DROP_FISH_WHO_ARE_ABOUT_TO_BEAR_A_FISH)
+                        .forEach { internalTimerValue ->
+                            it[internalTimerValue.dec()] = it.getValue(internalTimerValue)
+                        }
+
+                    it.computeIfPresent(
+                        INTERNAL_TIMER_FOR_FISH_WHO_JUST_BORE_ANOTHER_FISH
+                    ) { _, currentNumberOfFish -> currentNumberOfFish + numberOfNewbornFish }
+
+                    it[INTERNAL_TIMER_FOR_NEWBORN_FISH] = numberOfNewbornFish
                 }
                 .toMap()
         )
     }
 
     data class Fish(val internalTimer: Int)
+
+    companion object {
+        private const val DROP_FISH_WHO_ARE_ABOUT_TO_BEAR_A_FISH = 1
+        private const val INTERNAL_TIMER_FOR_FISH_WHO_JUST_BORE_ANOTHER_FISH = 6
+        private const val INTERNAL_TIMER_FOR_NEWBORN_FISH = 8
+        private const val MIN_INTERNAL_TIMER = 0
+        private const val MAX_INTERNAL_TIMER = INTERNAL_TIMER_FOR_NEWBORN_FISH
+    }
 }
